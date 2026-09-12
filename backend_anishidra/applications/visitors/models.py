bef3faadb055767ddcb4a58e5ab7061361e85aae
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-12 17:32:56 +0100

    Configure urls for project

--- backend_anishidra/applications/visitors/models.py
+++ backend_anishidra/applications/visitors/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Visitor(models.Model):
     ip_address = models.GenericIPAddressField(null=True, blank=True)
-    user_agent = models.TextField(blank=True)
+    user_agent = models.TextField(blank=True, null=True)
     visited_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"{self.ip_adress} - {self.visited_at}"

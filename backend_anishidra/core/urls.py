33fdedc849d62223309cbc04138cd99fa8d1df29
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-12 17:24:14 +0100

    Configure urls for project

--- backend_anishidra/core/urls.py
+++ backend_anishidra/core/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('api/contact/', include('applications.contact.urls')),
     path('api/management/', include('applications.management.urls')),
     path('api/visitors/', include('applications.visitors.urls')),
-    path('api/visitors/', include('applications.visitors.healthurls')),
+    path('api/visitorsh/', include('applications.visitors.healthurls')),
 ]
 
 if settings.DEBUG:

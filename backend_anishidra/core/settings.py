75512d557d59e1895eaf1dba39675e689bd6856b
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-12 12:48:43 +0100

    Configure WhiteNoise in MIDDLEWARE and STATICFILES

--- backend_anishidra/core/settings.py
+++ backend_anishidra/core/settings.py
@@ -65,7 +65,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whilenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,7 +151,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"

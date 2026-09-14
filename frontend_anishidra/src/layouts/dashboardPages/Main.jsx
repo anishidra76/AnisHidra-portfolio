b1d1e6aff4432addf407ebf3d3160b8fcc478ae3
anishidra76 <anishidradeveloper76@gmail.com>
2026-09-14 12:26:20 +0100

    Images solving problem

--- frontend_anishidra/src/layouts/dashboardPages/Main.jsx
+++ frontend_anishidra/src/layouts/dashboardPages/Main.jsx
@@ -1877,7 +1877,9 @@ const saveProjects = async () => {
                 <img src={
                   about[0].image instanceof File
                     ? URL.createObjectURL(about[0].image)
-                    : about[0].image
+                    : about[0].image.startsWith('http')
+                    ? about[0].image
+                    : `${CLOUDINARY_URL}${about[0].image}`
                 }
                 alt="About Picture" />
               ) : (
